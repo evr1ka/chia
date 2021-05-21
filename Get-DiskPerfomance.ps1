0fe3cc903d7a3760d562c7c04b40733ae6ad820a
Emil Romanov <evr1ka@mail.ru>
2021-05-21 08:14:08 +0300

    First commit
    First commit

---
+++ Get-DiskPerfomance.ps1
@@ -0,0 +1,78 @@
+﻿cls
+$DiskFilter = "name= 'F:' "
+$Sleep=3
+$Idle1=$DiskTime1=$T1=$Idle2=$DiskTime2=$T2=1
+$DateStr = Get-Date -Format "yyyy.MM.dd HH.mm"
+$LogFile = "C:\Temp\$DateStr SystemCounters.csv"
+if (!(test-path $LogFile))
+	{"sep=;" > $LogFile	
+	# "Date;Time;PercentIdleTime;PercentDiskTime" >> $LogFile	
+    #"DateTime;PercentDiskReadTime;PercentDiskWriteTime;PercentRamTime"
+    	
+    "DateTime;PercentDiskReadTime;PercentDiskWriteTime;RamAvailable;PercentIdleTime;PercentDiskTime;RamDeviation;RamAvaiableMb2;RamAvaiableMb1" >> $LogFile		
+	}
+$FullRam = 32*1024
+
+function AddSpace($In)
+    {$out = $In
+    if     ((0 -le $In) -and ($In -le 9))          {$out = "     $In"}
+    elseif ((10 -le $In) -and ($In -le 99))        {$out = "    $In"}
+    elseif ((100 -le $In) -and ($In -le 999))      {$out = "   $In"}
+    elseif ((1000 -le $In) -and ($In -le 9999))    {$out = "  $In"}
+    elseif ((10000 -le $In) -and ($In -le 99999))  {$out = " $In"}
+    elseif ((100000 -le $In) -and ($In -le 999999)){$out = "$In"}
+    else {$out = "     $In"}
+    return $out
+    }
+
+For($i=0;$i -le 100; $i=0)
+    {$Date = Get-Date
+    $Disk = Get-WmiObject -class Win32_PerfRawData_PerfDisk_LogicalDisk -filter $DiskFilter
+	$RamAvaiableMb1 = $(Get-Counter -Counter "\Память\Доступно МБ").CounterSamples.CookedValue
+    [Double]$Idle1 = $Disk.PercentIdleTime
+    [Double]$DiskTime1 = $Disk.PercentDiskTime
+    [Double]$DiskReadTime1 = $Disk.PercentDiskReadTime
+    [Double]$DiskWriteTime1 = $Disk.PercentDiskWriteTime
+	
+    [Double]$T1 = $Disk.TimeStamp_Sys100NS
+
+    start-Sleep $Sleep
+
+    $Disk = Get-WmiObject -class Win32_PerfRawData_PerfDisk_LogicalDisk -filter $DiskFilter
+	$RamAvaiableMb2 = $(Get-Counter -Counter "\Память\Доступно МБ").CounterSamples.CookedValue
+    [Double]$Idle2 = $Disk.PercentIdleTime
+    [Double]$DiskTime2 = $Disk.PercentDiskTime
+    [Double]$DiskReadTime2 = $Disk.PercentDiskReadTime
+    [Double]$DiskWriteTime2 = $Disk.PercentDiskWriteTime
+
+    [Double]$T2 = $Disk.TimeStamp_Sys100NS
+
+    $PercentIdleTime =[math]::Round((($Idle2 - $Idle1) / ($T2 - $T1)) * 100)
+    $PercentDiskTime =[math]::Round((($DiskTime2 - $DiskTime1) / ($T2 - $T1)) * 100)
+
+    $PercentDiskReadTime =[math]::Round((($DiskReadTime2 - $DiskReadTime1) / ($T2 - $T1)) * 100)
+    $PercentDiskWriteTime =[math]::Round((($DiskWriteTime2 - $DiskWriteTime1) / ($T2 - $T1)) * 100)
+
+	$RamDeviation = (($RamAvaiableMb2 - $RamAvaiableMb1))
+    #$PercentRamTime = [math]::Round((($FullRam-$RamAvaiableMb2)/$FullRam) * 100)
+    $RamAvailableGB = (($RamAvaiableMb2)/1024)
+
+    $Date = Get-Date
+    Write-host "$Date" -f White -NoNewline
+    #Write-host " $PercentIdleTime" -f Red -NoNewline
+	#Write-host " $PercentDiskTime" -f DarkRed  -NoNewline    
+    Write-host " $(AddSpace($PercentDiskReadTime))" -f Green  -NoNewline
+    Write-host " $(AddSpace($PercentDiskWriteTime))" -f Yellow  -NoNewline
+    Write-host " $(AddSpace($PercentIdleTime)) " -f Red  -NoNewline
+    '{0:N2}' -f  $RamAvailableGb
+    #Write-host " $RamAvaiableMb2" -f Blue  -NoNewline 
+    #If ($RamDeviation -lt 0)
+    #    {Write-host " $RamDeviation" -f DarkRed
+    #    }
+    #else{
+    #    Write-host " $RamDeviation" -f Red   
+	#    }
+    #"$($Date.ToShortDateString());$($Date.ToShortTimeString());$PercentIdleTime;$PercentDiskTime;$PercentRamTime;$RamAvaiableMb2;$RamAvaiableMb1" >> $LogFile
+    #"$Date;$PercentIdleTime;$PercentDiskTime;$PercentDiskReadTime;$PercentDiskWriteTime;$RamDeviation;$PercentRamTime;$RamAvaiableMb2;$RamAvaiableMb1" >> $LogFile
+    "$Date;$PercentDiskReadTime;$PercentDiskWriteTime;$RamAvailable;$PercentIdleTime;$PercentDiskTime;$RamDeviation;$RamAvaiableMb2;$RamAvaiableMb1" >> $LogFile		
+}
---
+++ PlotGtoE_bucket16_tr4.ps1
@@ -0,0 +1,62 @@
+﻿#Ver_1.1 Emil Romanov  2021.05.07
+#set-executionpolicy remotesigned
+$iCount = 100
+# threads: 1-8
+$threads=4
+# K: 1-5
+$bucket = 16
+$Disk = 'G:'
+#switch ($threads)
+#	{1 		{$ram = 1700}
+#	 2 		{$ram = 1694.5}	# 256 корзин - 1700 	
+#	 4 		{$ram = 1704} # 128 корзин - 1700
+#	 6 		{$ram = 1708} # 64 корзин - 1700
+#	 8 		{$ram = 1712} # 32 корзин - 1700	 	
+#	 default{$ram = 1700}
+#	}
+switch ($bucket)
+	{256 		{$ram = 1700}	# 256 корзин - 1700 
+	 128		{$ram = 3390}	# 128 корзин - 1700 3400	
+	 64 		{$ram = 7400}	# 64  корзин - 1700 6800
+	 32 		{$ram = 14800}	# 32  корзин - 1700 13600
+     16 		{$ram = 29600}	# 32  корзин - 1700 27200
+	 default{$ram = 3390}
+	}	
+	
+
+# Delete all temporary files #Get-ChildItem -Path F:\Temp -Include *.* -File -Recurse | foreach { $_.Delete()}
+#cd "C:\Users\romanove\AppData\Local\chia-blockchain\app-1.1.4\resources\app.asar.unpacked\daemon"
+cd $env:LOCALAPPDATA
+cd .\chia-blockchain\
+$appDir = ls "app-*"
+cd "$($appDir.Name)\resources\app.asar.unpacked\daemon"
+.\chia.exe version
+# 256 корзин - 1700, ~128 корзин - ~3400, 64 корзины - ~6800, 32 корзины - ~13600 на плот минимум
+# Разработчики пишут, что максимально может использоваться - 6750 MB на один плот, при k=32 со 128 корзинами.
+# Start plotting
+#3389 is the perfect amount if you are using 2 threads. I have found that 4 threads requires a minimum of 3408; 6 threads 3416; 8 threads 3424.
+if (!(test-path "D:\ChiaPS.csv"))
+	{"sep=;" > "D:\ChiaPS.csv"	
+	 "Date;Time;Plot;Status;R;Buc;Ram;I" >> "D:\ChiaPS.csv"		
+	}
+for($i=1;$i -le $iCount;$i++)
+	{if (!(test-path "D:\chiastop.txt"))
+		{$Date = Get-Date
+		$WindowTitle = "$Date Plot F to $Disk $i"
+		$host.ui.RawUI.WindowTitle = "$WindowTitle"
+		$OutStr = "$($Date.ToShortDateString());$($Date.ToShortTimeString());$WindowTitle;Start;$threads;$bucket;$ram;$i"
+		$OutStr >> "D:\ChiaPS.csv"
+		Write-Host $OutStr -f green
+		.\chia.exe plots create -k 32 -n 1 -t "F:\Temp" -d "$Disk\Plot" -b $ram -u $bucket -r $threads
+		$Date = Get-Date
+		$OutStr = "$($Date.ToShortDateString());$($Date.ToShortTimeString());$WindowTitle;Finish;$threads;$bucket;$ram;$i"
+		$OutStr >> "D:\ChiaPS.csv"
+		Write-Host $OutStr -f green
+		Write-Host "Press CTRL+C for break. Pause 5 minutes" -f green
+		Start-Sleep -Seconds 300
+		}
+    else{
+        exit
+        }
+	}
+Read-Host -Prompt "press enter to exit"
